af842ffab225f53fe28ae779c4e606f952df3733
Gabriel8546 <grf1620@gmail.com>
2021-04-12 23:27:46 -0600

    agregue varas

--- ProyectoDatos/src/proyectodatos/ProyectoDatos.java
+++ ProyectoDatos/src/proyectodatos/ProyectoDatos.java
@@ -25,21 +25,25 @@ public class ProyectoDatos {
             switch (opcion) {
 
                 case 1:
-                    String nombre = JOptionPane.showInputDialog("");
-                    int ID = Integer.parseInt(JOptionPane.showInputDialog(""));
+                    String nombre = JOptionPane.showInputDialog("Dgite el nombre del competidor ");
+                    int ID = Integer.parseInt(JOptionPane.showInputDialog("Digite el "));
                     String patrocinador = JOptionPane.showInputDialog("Ingrese patrocinador"); 
-                    int tiempoVuelta1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese tiempo de la primera vuelta"));
-                    int tiempoVuelta2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese tiempo de la segunda vuelta"));
-                    int tiempoVuelta3 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese tiempo de la ultima vuelta"));
+                    int tiempoVuelta1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese tiempo del primer  dia de rally del corredor"));
+                    int tiempoVuelta2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese tiempo del segundo dia de rally del corredor"));
+                    int tiempoVuelta3 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese tiempo del trecer dia de rally del corredor"));
                     
                     break;
                 case 2:
                          
                     break;
                 case 3:
-                    
+                       
                     break;
                 case 4:
+                    JOptionPane.showMessageDialog(null,"Hasta pronto!!!");
+                    break;
+                 default:
+                      JOptionPane.showMessageDialog(null,"Digite una opcion valida");
                     break;
             }
         } while (opcion !=4);
